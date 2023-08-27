fb70e6d6ebc137f5b35881e98163d16541773434
silqwer <silqwer@gmail.com>
2023-08-28 08:27:11 +0900

    style: window 95 느낌의 한글 폰트 추가 (#7)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,14 @@ export default function Home() {
     <main>
       <h1>Home</h1>
       <h1>홈</h1>
+
+      <h1 className={`font-[ChosunGu]`}>조선굴림</h1>
+      <h1 className="font-GowunBatang">고운바탕</h1>
+
+      <h1 className="font-notosans">노토산스</h1>
+      <h1 className="font-notosans">NotoSans</h1>
+      <h1 className="font-gulim">조선굴림</h1>
+      <h1 className="font-batang">고운바탕</h1>
     </main>
   );
 }
