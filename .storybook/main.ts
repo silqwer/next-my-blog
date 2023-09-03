3de07c7a88febe90d02e4f222b8b9cbb23764f98
silqwer <silqwer@gmail.com>
2023-09-03 13:32:22 +0900

    chore. set storybook
    - svgr 설정

---
+++ .storybook/main.ts
@@ -0,0 +1,46 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    const imageRule = config.module?.rules?.find((rule) => {
+      const test = (rule as { test: RegExp }).test;
+
+      if (!test) {
+        return false;
+      }
+
+      return test.test(".svg");
+    }) as { [key: string]: any };
+
+    imageRule.exclude = /\.svg$/;
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    if (config.resolve) {
+      config.resolve.alias = {
+        "@": path.resolve(__dirname, "../src"),
+      };
+    }
+
+    return config;
+  },
+};
+export default config;
---
+++ src/stories/example/ExampleMenu.stories.tsx
@@ -0,0 +1,21 @@
+import React95Menu from "@/components/React95Menu";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Example/React95Menu",
+  component: React95Menu,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof React95Menu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: "Button",
+  },
+};
